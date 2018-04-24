ce5643c72e39d2c2803c8677b5ca339374bcd6c4
886390 <886390@PMG34INFLB20710.PUCMINAS.NET>
2018-04-24 13:51:33 -0300

    bug fix

--- src/main/java/br/pucminas/printerclient/Printer.java
+++ src/main/java/br/pucminas/printerclient/Printer.java
@@ -6,38 +6,43 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Printer {
-	
+public class Printer extends Thread {
+
 	int port;
-	
+
 	public Printer(int port) {
 		this.port = port;
 	}
 
 	@SuppressWarnings("resource")
-	public void start() throws IOException {
-		ServerSocket ss = new ServerSocket(port);
-		while (true) {
-			final Socket sock = ss.accept();
-			new Thread() {
-				@Override
-				public void run() {
-					try {
-						InputStreamReader iReader = new InputStreamReader(sock.getInputStream());
-						BufferedReader reader = new BufferedReader(iReader);
-						String message;
-						// As long as this reader is open, will take action the moment a message
-						// arrives.
-						while ((message = reader.readLine()) != null) {
-							System.out.println(message);
+	public void run() {
+		try {
+			ServerSocket ss = new ServerSocket(port);
+			while (true) {
+				final Socket sock = ss.accept();
+				new Thread() {
+					@Override
+					public void run() {
+						try {
+							InputStreamReader iReader = new InputStreamReader(sock.getInputStream());
+							BufferedReader reader = new BufferedReader(iReader);
+							String message;
+							// As long as this reader is open, will take action the moment a message
+							// arrives.
+							while ((message = reader.readLine()) != null) {
+								System.out.println(message);
+							}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
 						}
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
 					}
-				}
 
-			}.start();
+				}.start();
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 	}
 
