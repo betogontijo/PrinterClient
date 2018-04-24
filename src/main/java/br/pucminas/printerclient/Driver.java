4f4718b7b1410c27b83b1fec8447b4c76618681c
886390 <886390@PMG34INFLB20710.PUCMINAS.NET>
2018-04-24 14:08:30 -0300

    fix ip comparison

--- src/main/java/br/pucminas/printerclient/Driver.java
+++ src/main/java/br/pucminas/printerclient/Driver.java
@@ -48,7 +48,7 @@ public class Driver {
 		Radar radar = new Radar(port, port);
 		radar.start();
 		String localAddress = radar.getLocalAddress();
-//		new Printer(9000).start();
+		// new Printer(9000).start();
 		List<Peer> ips = new ArrayList<Peer>(radar.getPeers());
 
 		while (true) {
@@ -64,7 +64,13 @@ public class Driver {
 						cluster.sort(new Comparator<String>() {
 							@Override
 							public int compare(String o1, String o2) {
-								return o1.compareTo(o2);
+								String[] ips1 = o1.split("\\.");
+								String updatedIp1 = String.format("%3s.%3s.%3s.%3s", ips1[0], ips1[1], ips1[2],
+										ips1[3]);
+								String[] ips2 = o2.split("\\.");
+								String updatedIp2 = String.format("%3s.%3s.%3s.%3s", ips2[0], ips2[1], ips2[2],
+										ips2[3]);
+								return updatedIp1.compareTo(updatedIp2);
 							}
 						});
 						criticalSection = new BufferedWriter(
